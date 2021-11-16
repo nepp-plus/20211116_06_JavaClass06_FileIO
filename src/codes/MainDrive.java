2d99171fd2de21e534d325314139898e0f3d3fb4
nepp-plus <nepp.plus@gmail.com>
2021-11-16 17:03:47 +0900

    파일 읽어오기 구현중

--- src/codes/MainDrive.java
+++ src/codes/MainDrive.java
@@ -1,7 +1,10 @@
 package codes;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 
@@ -10,7 +13,57 @@ public class MainDrive {
 	public static void main(String[] args) {
 		
 		
-		writeToFile();
+//		writeToFile();
+		
+		readFile();
+		
+	}
+	
+	static void readFile() {
+		
+//		파일 LOAD (Input) 실습
+		
+//		1. 어디서 파일을 불러올건지? 경로 설정.
+		File myFile = new File("mytest.txt");
+		
+//		2. 파일 불러내기 클래스 활용 -> 파일이 지워져있으면? 예외 대응 try / catch 필요.
+		
+		try {
+			FileReader fr = new FileReader(myFile);
+			
+//			여러 글자를 뭉쳐서 String으로 읽어오게 하는 클래스. => 한번에 한 줄씩 불러내자.
+			BufferedReader br = new BufferedReader(fr);
+			
+			
+//			몇줄이나 읽어와야할지 알 수 없다. (횟수 불명확 반복)
+			while (true) {
+				
+				
+//				readLine은 IOException 추가 대비 필요.
+				String line =  br.readLine();
+				
+				if (line == null) {
+					
+//					더이상 읽을 내용이 없었다.
+//					반복 탈출.
+					System.out.println("끝까지 읽음");
+					break;
+					
+				}
+				
+				
+				System.out.println(line);
+				
+			}
+			
+			
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 	}
 	
