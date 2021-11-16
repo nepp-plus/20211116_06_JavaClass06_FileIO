7829a79ac509e3ae1e6bc4aac69c000ffd7cbd31
nepp-plus <nepp.plus@gmail.com>
2021-11-16 16:50:57 +0900

    실제 파일 저장

--- src/codes/MainDrive.java
+++ src/codes/MainDrive.java
@@ -1,11 +1,14 @@
 package codes;
 
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
+import java.io.IOException;
 
 public class MainDrive {
 	
 	public static void main(String[] args) {
+		
 		
 		writeToFile();
 		
@@ -19,9 +22,33 @@ public class MainDrive {
 		
 		File myFile = new File("mytest.txt");
 		
-//		2. 파일 작성 클래스 활용.
+//		2. 파일 작성 클래스 활용. => 위험소지 : try / catch로 엮어서 사용.
 		
-		FileWriter fw = new FileWriter(myFile);
+		try {
+			
+//			한 글자를 2byte 다룬다. -> 한번에 한글자씩만 처리함.
+			FileWriter fw = new FileWriter(myFile);
+			
+//			"안녕하세요" 문장을 통째로 다루게 도와주는 클래스 사용.
+			
+			BufferedWriter bw = new BufferedWriter(fw);
+			
+			
+//			실제 문장 기록 : bw를 이용하자.
+			bw.append("안녕하세요.");
+			
+//			파일은 다 쓰면 닫아주자. (다른곳에서도 사용 가능하게)
+			
+			bw.close();
+			fw.close();
+			
+			
+			
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 		
 		
